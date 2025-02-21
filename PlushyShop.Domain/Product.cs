e738786a7bbc3dc3b71cc9204307e235eb1cac33
Kristina_Brelova <krisbrelova838@gmail.com>
2025-02-22 03:43:46 +0500

    Создание классов для базы данных

---
+++ PlushyShop.Domain/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlushyShop.Domain
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Creator { get; set; }
+        public string CreatorId { get; set; }
+        [ForeignKey("CreatorId")]
+        public string Category { get; set; }
+        public string Info { get; set; }
+        public string Town { get; set; }
+        public double Price { get; set; }
+        public double DeliveryPrice { get; set; }
+    }
+}
